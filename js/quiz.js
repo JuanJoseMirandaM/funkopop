71009366d8d6dc9c23bbba4ef87b218d11ff0db9
Juan Jose Miranda <jjsmm97@gmail.com>
2020-07-14 17:04:35 -0400

    arreglando js

--- js/quiz.js
+++ js/quiz.js
@@ -1,58 +1,54 @@
-function prg01(value){
+function prg01(value,i){
     document.getElementById('resp01').style.display = 'block';
     if (value == 1) {
         document.getElementById('resp01_dialog').innerHTML = '¡Correcto!'
     }else{
         document.getElementById('resp01_dialog').innerHTML = '¡Incorrecto!'  
-    }
-    document.getElementById('btn01_01').setAttribute("class", "btn btn-danger btn-lg btn-block")  
-    document.getElementById('btn01_02').setAttribute("class", "btn btn-success btn-lg btn-block")  
-    document.getElementById('btn01_03').setAttribute("class", "btn btn-danger btn-lg btn-block")  
+        document.getElementById('btn01_0'+i).setAttribute("class", "btn btn-danger btn-lg btn-block")   
+    } 
+    document.getElementById('btn01_02').setAttribute("class", "btn btn-success btn-lg btn-block")   
     document.getElementById('btn01_01').setAttribute("disabled", "true")
     document.getElementById('btn01_02').setAttribute("disabled", "true")
     document.getElementById('btn01_03').setAttribute("disabled", "true")
 }
 
-function prg02(value){
+function prg02(value,i){
     document.getElementById('resp02').style.display = 'block';
     if (value == 1) {
         document.getElementById('resp02_dialog').innerHTML = '¡Correcto!'
     }else{
         document.getElementById('resp02_dialog').innerHTML = '¡Incorrecto!'  
-    }
-    document.getElementById('btn02_01').setAttribute("class", "btn btn-danger btn-lg btn-block")  
-    document.getElementById('btn02_02').setAttribute("class", "btn btn-success btn-lg btn-block")  
-    document.getElementById('btn02_03').setAttribute("class", "btn btn-danger btn-lg btn-block")  
+        document.getElementById('btn02_0'+i).setAttribute("class", "btn btn-danger btn-lg btn-block")   
+    } 
+    document.getElementById('btn02_01').setAttribute("class", "btn btn-success btn-lg btn-block")   
     document.getElementById('btn02_01').setAttribute("disabled", "true")
     document.getElementById('btn02_02').setAttribute("disabled", "true")
     document.getElementById('btn02_03').setAttribute("disabled", "true")
 }
 
-function prg03(value){
+function prg03(value,i){
     document.getElementById('resp03').style.display = 'block';
     if (value == 1) {
         document.getElementById('resp03_dialog').innerHTML = '¡Correcto!'
     }else{
-        document.getElementById('resp03_dialog').innerHTML = '¡Incorrecto!'  
-    }
-    document.getElementById('btn03_01').setAttribute("class", "btn btn-danger btn-lg btn-block")  
-    document.getElementById('btn03_02').setAttribute("class", "btn btn-success btn-lg btn-block")  
-    document.getElementById('btn03_03').setAttribute("class", "btn btn-danger btn-lg btn-block")  
+        document.getElementById('resp03_dialog').innerHTML = '¡Incorrecto!'
+        document.getElementById('btn03_0'+i).setAttribute("class", "btn btn-danger btn-lg btn-block")   
+    } 
+    document.getElementById('btn03_03').setAttribute("class", "btn btn-success btn-lg btn-block")   
     document.getElementById('btn03_01').setAttribute("disabled", "true")
     document.getElementById('btn03_02').setAttribute("disabled", "true")
     document.getElementById('btn03_03').setAttribute("disabled", "true")
 }
 
-function prg04(value){
+function prg04(value,i){
     document.getElementById('resp04').style.display = 'block';
     if (value == 1) {
         document.getElementById('resp04_dialog').innerHTML = '¡Correcto!'
     }else{
-        document.getElementById('resp04_dialog').innerHTML = '¡Incorrecto!'  
-    }
-    document.getElementById('btn04_01').setAttribute("class", "btn btn-danger btn-lg btn-block")  
-    document.getElementById('btn04_02').setAttribute("class", "btn btn-success btn-lg btn-block")  
-    document.getElementById('btn04_03').setAttribute("class", "btn btn-danger btn-lg btn-block")  
+        document.getElementById('resp04_dialog').innerHTML = '¡Incorrecto!'
+        document.getElementById('btn04_0'+i).setAttribute("class", "btn btn-danger btn-lg btn-block")   
+    } 
+    document.getElementById('btn04_02').setAttribute("class", "btn btn-success btn-lg btn-block")   
     document.getElementById('btn04_01').setAttribute("disabled", "true")
     document.getElementById('btn04_02').setAttribute("disabled", "true")
     document.getElementById('btn04_03').setAttribute("disabled", "true")
